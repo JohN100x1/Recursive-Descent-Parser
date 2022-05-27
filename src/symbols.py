596d10a4b6c673796b5c617f66addd18cf3abf39
JohN100x1 <JohN100x1@hotmail.co.uk>
2022-05-27 18:56:18 +0100

    feat: added symbol subclasses

--- src/symbols.py
+++ src/symbols.py
@@ -1,10 +1,60 @@
-class Symbol:
-    pass
+from abc import ABC, abstractmethod
+from typing import Type
+
+
+class Symbol(ABC):
+    @property
+    def name(self) -> str:
+        return self.__class__.__name__
+
+
+# Production
+class Production:
+    def __init__(self, *args: Type[Symbol]):
+        self.body = list(args)
+
+    def contents(self) -> str:
+        return ", ".join(s.__name__ for s in self.body)
 
 
 class TerminalSymbol(Symbol):
-    pass
+    def __init__(self, lexeme: str):
+        self.lexeme = lexeme
 
 
 class NonTerminalSymbol(Symbol):
+    @property
+    @abstractmethod
+    def productions(self) -> list[Production]:
+        ...
+
+
+# Terminals
+class IntegerLiteral(TerminalSymbol):
     pass
+
+
+class PlusLiteral(TerminalSymbol):
+    def __init__(self, lexeme: str = "+"):
+        super().__init__(lexeme)
+
+
+class MultLiteral(TerminalSymbol):
+    def __init__(self, lexeme: str = "*"):
+        super().__init__(lexeme)
+
+
+# Non-terminals
+class TermSymbol(NonTerminalSymbol):
+    @property
+    def productions(self) -> list[Production]:
+        return [
+            Production(FactorSymbol, PlusLiteral, FactorSymbol),
+            Production(FactorSymbol),
+        ]
+
+
+class FactorSymbol(NonTerminalSymbol):
+    @property
+    def productions(self) -> list[Production]:
+        return [Production(IntegerLiteral)]
