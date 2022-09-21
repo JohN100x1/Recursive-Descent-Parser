6b7debb5b21a6539086bcf173d85a54d623d2e7c
JohN100x1 <JohN100x1@hotmail.co.uk>
2022-09-22 00:01:25 +0100

    feat: added CHANGELOG.md

--- src/dsl/models/representables/operands.py
+++ src/dsl/models/representables/operands.py
@@ -1,5 +1,5 @@
 from abc import abstractmethod
-from typing import Any, Optional
+from typing import Any
 
 from dsl.models.exceptions import DSLException, DSLRuntimeError
 from dsl.models.representables import Representable
